d5c3b014f51c7330255610fd151d90323abaed01
Nellia Salimova <salimova.nellia@gmail.com>
2020-06-29 18:21:45 +0300

    Changing a recipe model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,7 @@
 export class Recipe {
-  public name: string;
-  public description: string;
-  public imgRef: string;
-
-  constructor(name: string, description: string, imgRef: string) {
-    this.name = name;
-    this.description = description;
-    this.imgRef = imgRef;
+  constructor(
+    public name: string,
+    public description: string,
+    public imgRef: string) {
   }
 }
