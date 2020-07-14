60777072e77dc19bd0ae65d6421dbd08befa3257
Nellia Salimova <salimova.nellia@gmail.com>
2020-07-14 22:10:48 +0300

    Implementation adding of ingredients

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -18,4 +18,7 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onIngredientAdded(addedIngredient: Ingredient) {
+    this.ingredients.push(addedIngredient);
+  }
 }
